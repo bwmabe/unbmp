fb21528b3c7af7437050bc7faeaeee3f361e3226
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-03-22 17:37:49 -0400

    added doc comments; fixed a typo; verbose testing

--- prepare-test.sh
+++ prepare-test.sh
@@ -6,8 +6,8 @@ rm -r test/test_files
 mkdir test/test_dir
 mkdir test/test_files
 
-cp test/test_images/* test/test_dir/
-cp test/test_images/* test/test_files/
+cp -r test/test_images/* test/test_dir/
+cp -r test/test_images/* test/test_files/
 
 # Globbing should work?
 echo ""
